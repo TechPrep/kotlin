96e5a345c2c9c6f00e7c9a6856aa1fbf53e88aab
v1.gandhi <v1.gandhi@samsung.com>
2019-03-13 13:54:33 -0400

    Trial from machine

--- app/src/main/java/com/study/kotlin/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-    //trial
+    //trial from machine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
